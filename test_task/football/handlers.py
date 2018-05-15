7129c8c514a9c99e82eded4467d0ae8f75311075
petar.n <petar.n@delasport.com>
2018-05-15 10:08:50 +0300

    some additional adjustments

--- test_task/football/handlers.py
+++ test_task/football/handlers.py
@@ -25,11 +25,22 @@ def handle_received_source(source, league_obj, team_obj, event_obj, time_func, d
             # Fill the teams & events tables with source
             if len(source[date][league]):
                 for event in source[date][league]:
+                    event_id = event["event_id"]
                     team1 = event["home_team"]
                     team2 = event["away_team"]
                     event_title = event["event_name"]
                     start_time = event["start_date"]
                     event_status = event["status"]
+                    live_minute, live_score_home, live_score_away = 0, 0, 0
+
+                    if period == 'live':
+                        live_minute = event["liveScore"]["live_minute"]
+                        live_score_home = event["liveScore"]["home_team_score"]
+                        live_score_away = event["liveScore"]["away_team_score"]
+                    elif period == 'finished':
+                        live_score_home = event["liveScore"]["home_team_score"]
+                        live_score_away = event["liveScore"]["away_team_score"]
+
 
                     if not team_obj.objects.filter(title=team1).exists():
                         t1 = team_obj.objects.create(title=team1, country=country, league=league_title,
@@ -48,3 +59,27 @@ def handle_received_source(source, league_obj, team_obj, event_obj, time_func, d
                             e.save()
                         else:
                             continue
+                    elif period == 'live':
+                        if event_obj.objects.filter(event_id=event_id).exists():
+                            ev = event_obj.objects.filter(event_id=event_id)
+
+                            if ev.live_minute != live_minute:
+                                ev.live_minute = live_minute
+                                ev.save()
+                            if ev.live_score_home != live_score_home:
+                                ev.live_score_home = live_score_home
+                                ev.save()
+                            if ev.live_score_away != live_score_away:
+                                ev.live_score_away = live_score_away
+                                ev.save()
+                        else:
+                            e = event_obj.objects.create(title=event_title, country=country, status=event_status,
+                                                         live_minute=live_minute, live_score_home=live_score_home,
+                                                         live_score_away=live_score_away,
+                                                         created_at=date_func.now(), event_id=event_id)
+                            e.save()
+                    else:
+                        if not event_obj.objects.filter(title=event_title, start_time=start_time).exists():
+                            e = event_obj.objects.create(title=event_title, country=country, status=event_status,
+                                                         start_time=start_time, final_score_home=live_score_home, final_score_away=live_score_away)
+                            e.save()
